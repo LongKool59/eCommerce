1e1c3a12e2aa3bf33d4950f05c2986b15d14bff1
Mhieu181120 <75715594+Mhieu181120@users.noreply.github.com>
2022-01-08 15:33:34 +0700

    + Hieu (#32)
    + Thêm 1 vài data bên trang chủ
    + Chỉnh cái đồng hồ bên trnag chi tiết
    + Fluent

--- eCommerce/Global.asax.cs
+++ eCommerce/Global.asax.cs
@@ -18,7 +18,6 @@ namespace eCommerce
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            UpdateTrangThai();
 
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
@@ -27,6 +26,8 @@ namespace eCommerce
             NotificationComponents NC = new NotificationComponents();
 
             NC.RegisterNotification(true);
+            UpdateTrangThai();
+
         }
 
         protected void Session_Start(object sender, EventArgs e)
@@ -40,18 +41,37 @@ namespace eCommerce
         DauGiaEntities db = new DauGiaEntities();
         public void UpdateTrangThai()
         {
-            //DateTime now = DateTime.Now;
-            //var hethan = db.DauGias.Where(m => m.NgayKetThuc <= now).ToList();
-            //foreach(var dg in hethan)
-            //{
-            //    var ten = db.TrangThaiDauGias.Where(m => m.TenTrangThai == "UnActive").SingleOrDefault();
-            //    CT_TrangThai tt = new CT_TrangThai();
-            //    tt.MaDauGia = dg.MaDauGia;
-            //    tt.MaTrangThai = ten.MaTrangThai;
-            //    tt.ThoiGian = dg.NgayKetThuc;
-            //    db.CT_TrangThai.Add(tt);
-            //    db.SaveChanges();
-            //}    
+            DateTime now = DateTime.Now;
+            var hethan = db.DauGias.Where(m => m.NgayKetThuc <= now).ToList();
+            var DauGia_DaXoa = from d in db.DauGias
+                               join ct in db.CT_TrangThai on d.MaDauGia equals ct.MaDauGia
+                               where ct.MaTrangThai == 2
+                               select d;
+
+
+            var DauGia = hethan.Where(p => !DauGia_DaXoa.Any(p2 => p2.MaDauGia == p.MaDauGia) && p.MaNguoiMua == null).ToList(); 
+            foreach (var dg in DauGia)
+            {
+                var ten = db.TrangThaiDauGias.Where(m => m.TenTrangThai == "UnActive").SingleOrDefault();
+                CT_TrangThai tt = new CT_TrangThai();
+                tt.MaDauGia = dg.MaDauGia;
+                tt.MaTrangThai = ten.MaTrangThai;
+                tt.ThoiGian = dg.NgayKetThuc;
+                db.CT_TrangThai.Add(tt);
+                db.SaveChanges();
+
+                var mucnang = db.MucNangs.Where(m=>m.MaDauGia == dg.MaDauGia).ToList();
+                if(mucnang.Count()>0)
+                {
+                    var max = db.MucNangs.Where(m => m.MaDauGia == dg.MaDauGia).Max(m => m.GiaTri);
+                    var mn = db.MucNangs.Where(m => m.GiaTri == max && m.MaDauGia == dg.MaDauGia).SingleOrDefault();
+                    dg.MaNguoiMua = mn.MaNguoiDung;
+                    dg.GiaCuoi = mn.GiaTri;
+                    db.SaveChanges();    
+                }
+
+
+            }    
         }
         public class MyRegistry : Registry
         {
